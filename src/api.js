648935132730972df2e00c8bf04630c622d5bd77
MarioGoDevLike <marionassar57@gmail.com>
2023-04-13 21:54:59 +0300

    implemented search tab

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '32086d2791mshb53727e5edf0432p13670djsn88ded8d30b04',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
---
+++ src/components/currentWeather/currentweather.js
@@ -0,0 +1,42 @@
+import "./currentweather.css"
+
+const CurrentWeather = () => {
+  return (
+    <div className="weather">
+        <div className="top">
+            <div>
+                <p className="city">Belgrade</p>
+                <p className="weather-description">Sunny</p>
+            </div>
+        <img classname="weather-icon" src="icon/01d.png" alt="weather" />
+
+        </div>
+        <div className="bottom">
+                <p className="temperature">18°C</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label">Details:</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-label">22°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-label">2 m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-label">15%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-label">15 hPa</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CurrentWeather;
